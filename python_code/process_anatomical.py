45daffebe120eb4b3c667861933d6d11f495cf91
Jeff Stout <stoutjd@nih.gov>
2020-04-07 17:49:29 -0400

    Data checks and removed test info to another file

--- python_code/process_anatomical.py
+++ python_code/process_anatomical.py
@@ -53,21 +53,7 @@ def compile_fs_process_list(info):
     if info.recon3:
         process_steps.append('recon-all -autorecon3 -s {}'.format(info.subjid))
     return process_steps        
-
-
-# def compile_process_list(info):
-#     '''Main function for processing freesurfer and source level setup'''
-#     if ('brainmask.mgz' not in info.fs_mri_contents:
-#         print('Brainmask not present: Adding autorecon1 to process list')
-#     if ('lh.pial' not in info.fs_surf_contents) | ('rh.pial' not in info.fs_surf_contents):
-#         print('Left or Right hemi not present: Running autorecon2')
-#     if ('lh.aparc.annot' not in info.fs_label_contents) | ('rh.aparc.annot' not in info.fs_label_contents):
-#         print('Labels not present: Running autorecon3')
-#     if ('lh.mid' not in info.fs_surf_contents) | ('rh.mid' not in info.fs_surf_contents):
-#         print('Running mne.setup_source_space()')        
-#         from mne import setup_source_space
-#         setup_source_space(subject=info.subjid, subjects_dir=subjects_dir, n_jobs=1)   
-        
+      
     
 if __name__=='__main__':
     import sys
@@ -102,57 +88,7 @@ if __name__=='__main__':
     if args.run_unprocessed: info.run_unprocessed=True
     
 
-    
 
-def test_inputs():
-    subjid='APBWVFAR_fs'
-    subjects_dir=os.path.join(os.environ['HOME'],'hv_proc/MRI')
-    info=anat_info(subjid=subjid, SUBJECTS_DIR=subjects_dir)
-    assert info.subjid==subjid
-    assert info.subjects_dir==subjects_dir
-    assert info.recon1==False
-    assert info.recon2==False
-    assert info.recon3==False
-    assert info.setup_source==False
-    return info
-    
-
-def test_compile_fs_process_list():
-    info=test_inputs()
-    info.run_unprocessed = True
-    #Full list
-    assert compile_fs_process_list(info) == []
-    info.fs_label_contents.remove('lh.aparc.annot')
-    info.fs_label_contents.remove('rh.aparc.annot')
-    assert compile_fs_process_list(info) == ['recon-all -autorecon3 -s APBWVFAR_fs']
-    info=test_inputs() 
-    info.run_unprocessed = True 
-    info.fs_surf_contents.remove('lh.pial')
-    assert compile_fs_process_list(info) == ['recon-all -autorecon2 -s APBWVFAR_fs',
-                                             'recon-all -autorecon3 -s APBWVFAR_fs']
-    info=test_inputs()
-    info.run_unprocessed = True
-    info.fs_mri_contents.remove('brainmask.mgz')
-    #Process All
-    assert compile_fs_process_list(info) == ['recon-all -autorecon1 -s APBWVFAR_fs',
-                                             'recon-all -autorecon2 -s APBWVFAR_fs',
-                                             'recon-all -autorecon3 -s APBWVFAR_fs']
-    info=test_inputs()
-    info.recon1=True
-    assert compile_fs_process_list(info) == ['recon-all -autorecon1 -s APBWVFAR_fs']
-
-    info=test_inputs()
-    info.recon2=True   
-    assert compile_fs_process_list(info) == ['recon-all -autorecon2 -s APBWVFAR_fs']
-    
-    info=test_inputs()
-    info.recon3=True   
-    assert compile_fs_process_list(info) == ['recon-all -autorecon3 -s APBWVFAR_fs']
-    
-   
-    
-    
-    
     
 
 
