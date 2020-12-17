a7781c088b78465b632fe67e59df8c5372d27584
Jeff Stout <stoutjd@nih.gov>
2020-12-17 09:47:56 -0500

    Initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="enigma", # Replace with your own username
+    version="0.0.1",
+    author="Jeff Stout",
+    author_email="stoutjd@nih.gov",
+    description="Package to calculate metrics for the Enigma MEG consortium",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jstout211/enigma",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: None <<< FIX",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
