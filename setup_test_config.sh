3f6b6e7b30192fd5c8712f84d0e7c5f48a7dc4c4
Jeff Stout <stoutjd@nih.gov>
2020-12-23 16:26:24 -0500

    Minimal test config using conda enivronment

---
+++ setup_test_config.sh
@@ -0,0 +1,26 @@
+conda create -n test_enigma -y python=3.7 pip
+conda activate test_enigma
+
+env_dir=$(conda info | grep "envs directories" | awk '{print $4}')
+env_dir=$env_dir/test_enigma
+
+conda install pip python=3.7 -y
+cd ~/src
+
+tmp = $(basename $(dirname $(pwd)))
+if [[ tmp -eq "enigma" ]]
+then
+	echo Do not run this from the enigma repo
+	echo It will download another version
+	exit(1)
+fi
+
+git clone git@tako:enigma
+pip install ./enigma
+pip install -r enigma/requirements.txt
+
+cd enigma
+ln -s /data/test_data
+
+pytest
+
