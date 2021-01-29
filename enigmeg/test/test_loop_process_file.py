07083a752478e72fe8cb1bf9ef1da17db2eb53fd
Jeff Stout <stoutjd@nih.gov>
2021-01-29 17:54:50 -0500

    Test passes for csv file input processing

--- enigmeg/test/test_loop_process_file.py
+++ enigmeg/test/test_loop_process_file.py
@@ -10,6 +10,7 @@ import pandas as pd
 import enigmeg
 import mne
 import os, os.path as op
+import shutil
 
 import pytest
 from enigmeg.test_data import loop_test_data 
@@ -48,23 +49,37 @@ def parse_inputs(proc_file):
     proc_dframe = proc_dframe[keep_idx]
     
     for idx, dseries in proc_dframe.iterrows():
-        # print(idx)
         print(dseries)
+        
+        dseries['output_dir']=op.expanduser(dseries['output_dir'])
         
         from types import SimpleNamespace
         info = SimpleNamespace()
         info.SUBJECTS_DIR = dseries['fs_subjects_dir']
         
-        info.outfolder = dseries['output_dir']
-        info.bem_sol_filename = op.join(info.outfolder, dseries['subject'], 
-                                        'source_space-src.fif')
-        info.src_filename = op.join(info.outfolder, dseries['subject'], 
-                                    'bem_sol-sol.fif')
+        info.outfolder = op.join(dseries['output_dir'], dseries['subject'])
+        info.bem_sol_filename = op.join(info.outfolder, 'bem_sol-sol.fif') 
+        info.src_filename = op.join(info.outfolder, 'source_space-src.fif')
+        
 
         os.environ['SUBJECTS_DIR']=dseries['fs_subjects_dir']
         
         #Determine if meg_file_path is a full path or relative path
+        if not op.isabs(dseries['meg_file_path']):
+            if op.isabs(dseries['meg_top_dir']):
+                dseries['meg_file_path'] = op.join(dseries['meg_top_dir'], 
+                                                   dseries['meg_file_path'])
+            else:
+                raise ValueError('This is not a valid path')
         
+        #Perform the same check on the emptyroom data
+        if not op.isabs(dseries['eroom_file_path']):
+            if op.isabs(dseries['meg_top_dir']):
+                dseries['eroom_file_path'] = op.join(dseries['meg_top_dir'], 
+                                                   dseries['eroom_file_path'])
+            else:
+                raise ValueError('This is not a valid path')        
+            
         inputs = {'filename' : dseries['meg_file_path'],
                   'subjid' : dseries['subject'],
                   'trans' : dseries['trans_file'],
@@ -73,9 +88,6 @@ def parse_inputs(proc_file):
                   'emptyroom_filename' : dseries['eroom_file_path']}
         main(**inputs)
         
-    
-
-
 def test_process_file(tmpdir):
     '''Generate a csv file and use this as input for the config file loop
     Loop over all entries in a tab separated csv file'''
@@ -85,6 +97,8 @@ def test_process_file(tmpdir):
     
     test_dframe = pd.read_csv(test_process_file, delimiter='\t')
     
+    output_dir = tmpdir #op.expanduser('~/Desktop/TEMP')
+    
     # Process Elekta info
     elekta_dat = datasets().elekta    
     test_dframe.loc[2,'subject'] = elekta_dat['subject']
@@ -92,24 +106,39 @@ def test_process_file(tmpdir):
     test_dframe.loc[2, 'meg_top_dir'] = loop_test_data.__path__[0]
     test_dframe.loc[2, 'meg_file_path'] = 'short_elekta_rest.fif'
     test_dframe.loc[2, 'eroom_file_path'] = 'short_elekta_eroom.fif'
-    test_dframe.loc[2, 'output_dir'] = '~/Desktop/TEMP' #<<<<<<<<<<<<< Change to tmpdir
+    test_dframe.loc[2, 'output_dir'] = output_dir
     test_dframe.loc[2, 'line_freq'] = 50
     test_dframe.loc[2, 'trans_file'] = elekta_dat['trans']
+    enigma_subj_dir = op.join(output_dir, elekta_dat['subject'])
+    if not op.exists(enigma_subj_dir):
+                     os.mkdir(enigma_subj_dir)
+    shutil.copy(elekta_dat['src'], enigma_subj_dir)
+    shutil.copy(elekta_dat['bem'], enigma_subj_dir)
     
     # Process CTF info
     ctf_dat = datasets().ctf
     test_dframe.loc[3,'subject'] = ctf_dat['subject']
     test_dframe.loc[3, 'fs_subjects_dir'] = ctf_dat['SUBJECTS_DIR']
     test_dframe.loc[3, 'meg_top_dir'] = loop_test_data.__path__[0]
-    test_dframe.loc[3, 'meg_file_path'] = 'short_ctf_rest.fif'
-    test_dframe.loc[3, 'eroom_file_path'] = 'short_ctf_eroom.fif'
-    test_dframe.loc[3, 'output_dir'] = '~/Desktop/TEMP' #<<<<<<<<<<< Change to tmpdir
+    test_dframe.loc[3, 'meg_file_path'] = 'short_ctf_rest.ds'
+    test_dframe.loc[3, 'eroom_file_path'] = 'short_ctf_eroom.ds'
+    test_dframe.loc[3, 'output_dir'] = output_dir
     test_dframe.loc[3, 'line_freq'] = 60        
     test_dframe.loc[3, 'trans_file'] = ctf_dat['trans']
+    enigma_subj_dir = op.join(output_dir, ctf_dat['subject'])
+    if not op.exists(enigma_subj_dir):
+                     os.mkdir(enigma_subj_dir)
+    shutil.copy(ctf_dat['src'], enigma_subj_dir)
+    shutil.copy(ctf_dat['bem'], enigma_subj_dir)
     
-    output_csv = op.join('~/Desktop/TEMP', 'process.csv')  #<<<<<<<<<<<< Change to tmpdir
+    output_csv = op.join(output_dir, 'process.csv')  
     test_dframe.to_csv(output_csv, sep='\t', index=False)
-        
+    
+    parse_inputs(output_csv)
+    
+    #Verify that the outputs have been created for the multiple inputs
+    assert op.exists(op.join(output_dir, elekta_dat['subject'], 'Band_rel_power.csv'))
+    assert op.exists(op.join(output_dir, ctf_dat['subject'], 'Band_rel_power.csv'))
     
     print(test_dframe)
     
