c788eae4b493f86fe5d59c5cf146f874b3a0711a
Jeff Stout <stoutjd@nih.gov>
2021-03-19 09:05:08 -0400

    refactor: changed from hard coded script to functionalized code

--- enigmeg/group/compile_alpha_histplots.py
+++ enigmeg/group/compile_alpha_histplots.py
@@ -9,52 +9,61 @@ Created on Thu Oct 15 07:41:59 2020
 import pandas as pd
 import numpy as np
 import os, os.path as op
-# os.chdir('/home/stoutjd/data/DEMO_ENIGMA/outputsBAK/enigma_outputs')
-
-input_dir='/home/stoutjd/CAMCAN_ageValpha'
-os.chdir(input_dir)
-
 import glob
-
-
-# file_list=glob.glob('????????_fs/Band_rel_power.csv')
-# file_list=glob.glob('CompiledResults/????????_Band_rel_power.csv')
-file_list=glob.glob('CompiledResults2/sub-CC*_Band_rel_power.csv')
-curr_file=file_list[0]
-dframe=pd.read_csv(curr_file, sep='\t')
-
-#dframe['HASH_ID']= op.dirname(curr_file)[0:8]
-dframe['HASH_ID']= op.basename(curr_file)[4:12]
-
-
-for curr_file in file_list[1:]:
-    print(curr_file)
-    curr_dframe = pd.read_csv(curr_file, sep='\t')
-    # curr_dframe['HASH_ID'] = op.dirname(curr_file)[0:8]
-    curr_dframe['HASH_ID']= op.basename(curr_file)[4:12]
-    dframe = dframe.append(curr_dframe)
-    
-    del curr_dframe
-
-dframe = dframe.rename(columns={'Unnamed: 0':'Parcel'})
-
 import seaborn as sns
-
-plot_frame = dframe[['AlphaPeak','HASH_ID', 'Parcel']]
-rois = ['pericalcarine-rh','postcentral-rh', 'bankssts-rh',
-'pericalcarine-lh','postcentral-lh', 'bankssts-lh']
-
-plot_frame=plot_frame[plot_frame.Parcel.isin(rois)]
-plot_frame.dropna(inplace=True)
-
-sns.histplot(data=plot_frame, x='Parcel', y='AlphaPeak')
-sns.displot(plot_frame, x="AlphaPeak", kind="kde", bw_adjust=2, hue='Parcel')
-
 import pylab
-fig=pylab.Figure(figsize=(6,8), dpi=300)  #I dont think this is necessary
-tmp=sns.displot(plot_frame, x="AlphaPeak", kind="kde", bw_adjust=2, hue='Parcel')
-tmp.savefig(op.join(input_dir, 'SensoryHists.png'), dpi=300)
+
+def compile_group_outputs(enigma_outputs_path=None):
+    '''Find all single subject enigma csv files and compile into a single
+    dataframe'''
+
+    os.chdir(enigma_outputs_path)
+    filenames = glob.glob('*/Band_rel_power.csv')
+    return filenames
+
+def return_multisubject_dframe(filenames):
+    '''Take a list of filenames 
+    Load the csv filenames into a dataframe
+    Stitch together single subject dataframes into one large dataframe'''
+    
+    list_of_dframes = list()
+    for fname in filenames:
+        print(fname)
+        subj_dframe = pd.read_csv(fname, sep='\t')       
+        subj_dframe['subject'] = fname.split('/')[0]
+        list_of_dframes.append(subj_dframe)
+        
+    group_dframe = pd.concat(list_of_dframes)
+    group_dframe = group_dframe.rename(columns={'Unnamed: 0':'Parcel'})
+    return group_dframe
+        
+        
+    
 
 
+def test_plots(dframe):
+    plot_frame = dframe[['AlphaPeak','subject', 'Parcel']]
+    rois = ['pericalcarine_1-rh','postcentral_1-rh', 'bankssts_1-rh',
+    'pericalcarine_1-lh','postcentral_1-lh', 'bankssts_1-lh']
+    
+    plot_frame=plot_frame[plot_frame.Parcel.isin(rois)]
+    plot_frame.dropna(inplace=True)
+    
+    sns.histplot(data=plot_frame, x='Parcel', y='AlphaPeak')
+    sns.displot(plot_frame, x="AlphaPeak", kind="kde", bw_adjust=2, hue='Parcel')
+    
 
-dframe.to_csv(op.join(input_dir, 'Compiled_bandrel.csv'), index=False)
+    # fig=pylab.Figure(figsize=(6,8), dpi=300)  #I dont think this is necessary
+    # tmp=sns.displot(plot_frame, x="AlphaPeak", kind="kde", bw_adjust=2, hue='Parcel')
+    # tmp.savefig(op.join(input_dir, 'SensoryHists.png'), dpi=300)
+
+def test_compile_group_outputs():
+    # from enigmeg import test_data    
+    #Hack replace value w/ import parameter
+    top_dir = '/data/test_data/GROUP/enigma_outputs'
+    
+    fnames = compile_group_outputs(top_dir)
+    assert len(fnames) == 61
+
+# if __name__ == '__main__':
+    #dframe.to_csv(op.join(input_dir, 'Compiled_bandrel.csv'), index=False)
