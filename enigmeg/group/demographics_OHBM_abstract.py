141abeae3719f10bcdcf07441db460b1dfdd130c
Jeff Stout <stoutjd@nih.gov>
2023-01-17 09:59:37 -0500

    Final used for abstract processing

---
+++ enigmeg/group/demographics_OHBM_abstract.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 14:34:01 2023
+
+@author: jstout
+"""
+
+import os, os.path as op
+demo_dir = '/home/jstout/src/enigma_OHBM/RESULTS_OHBM/DEMOG'
+topdir = demo_dir
+os.chdir(topdir)
+
+import pandas as pd
+
+# =============================================================================
+# Get age/gender from participants.tsv
+# Download the all_demographics folder from biowulf enigma/bids/all_demographics
+# =============================================================================
+# demo_dir = '/tmp/all_demographics'
+demo=dict(ICA_1='NIHstr_participants.tsv',
+      results_mous='mous_participants.tsv',
+      NIH_y='NIHyan_participants.tsv',
+      NIH_hvprotocol='NIHhv_participants.tsv', 
+      Omega = 'omega_participants.tsv')
+
+def load_demo(fname):
+    return pd.read_csv(op.join(demo_dir, fname), sep='\t')
+
+demo_list = []
+column_sel = ['participant_id', 'age', 'sex','SheetName']
+
+#NIHstr data
+tmp = load_demo('NIHstr_participants.tsv')
+tmp['SheetName']='ICA_1'
+tmp.rename(columns=dict(participant_age='age'), inplace=True)
+demo_list.append(tmp.loc[:,column_sel])
+
+#NIHy data
+# tmp = load_demo('NIHyan_participants.tsv')
+# tmp['SheetName']='NIH_y'
+# tmp.rename(columns=dict(participant_age='age'), inplace=True)
+# demo_list.append(tmp.loc[:,column_sel])
+
+#NIHhv data
+tmp = load_demo('NIHhv_participants.tsv')
+tmp['SheetName']='NIH_hvprotocol'
+demo_list.append(tmp.loc[:,column_sel])
+
+#Mous data
+tmp = load_demo('mous_participants.tsv')
+tmp['SheetName']='results_mous'
+demo_list.append(tmp.loc[:,column_sel])
+
+#Camcan data
+tmp = load_demo('cam_participants.tsv')
+tmp['SheetName']='results_camcan'
+demo_list.append(tmp.loc[:,column_sel])
+
+#Omega data
+tmp = load_demo('omega_participants.tsv')
+tmp['SheetName'] = 'omega'
+demo_list.append(tmp.loc[:,column_sel])
+
+#Cardiff data
+tmp = pd.read_csv('meguk_cardiff_participants.csv')
+tmp['SheetName'] = 'cardiff'
+tmp.rename(columns={'MEG-PART-ID':'participant_id',
+            'Sex':'sex',
+            'Age':'age'}, inplace=True)
+demo_list.append(tmp.loc[:,column_sel])
+
+
+demo_final = pd.concat(demo_list)
+demo_final.reset_index(drop=True, inplace=True)
+
+
+
+# =============================================================================
+# Merge demographic and ICA results
+# =============================================================================
+#ICAColumns
+#['idx', 'sub', 'type', 'eyeblink', 'Saccade', 'EKG', 'other',
+#       'SheetName', 'Scanner', 'Site', 'TaskType', 'Unnamed: 6']
+
+def munge_subjid(subj):
+    subj=str(subj)
+    if subj[0:4]!='sub-':
+        subj='sub-'+subj
+    return subj
+
+def fix_nihy_subjids(dframe):
+    'Specifically parse datasets from NIH_y - subject IDs need to conform'
+    for idx, row in dframe.iterrows():
+        if row.SheetName=='NIH_y':
+            print(f"Fixing ID {dframe.loc[idx]['participant_id']}")
+            if len(str(dframe.loc[idx]['participant_id']))==1:
+                dframe.loc[idx, 'participant_id']='sub-000'+str(dframe.loc[idx]['participant_id'])
+            elif  len(str(dframe.loc[idx,'participant_id']))==2:
+                dframe.loc[idx, 'participant_id']='sub-00'+str(dframe.loc[idx]['participant_id'])
+            else:
+                print(f'Error with {dframe.loc[idx]["participant_id"]}')
+    
+
+import copy
+all_dat = copy.deepcopy(demo_final)
+
+reject_idx=[]
+for idx,i in enumerate(all_dat.age):
+    try:
+        int(i)
+    except:
+        reject_idx.append(idx)
+        continue
+    if int(i) < 0:
+        reject_idx.append(idx)
+        
+    
+    
+all_dat.drop(reject_idx, inplace=True)
+
+
+# all_dat[['participant_id', 'type']]
+#Cleanup
+#Drop subjects without age
+all_dat=all_dat.dropna(subset=['age'])
+all_dat['age']=all_dat['age'].astype(int)
+all_dat['age'].hist()
+
+
+
+#Change coding of M/F for hv protocol
+hv_m_idx = all_dat[(all_dat.SheetName=='NIH_hvprotocol') & (all_dat.sex==1)].index
+hv_f_idx = all_dat[(all_dat.SheetName=='NIH_hvprotocol') & (all_dat.sex==2)].index
+all_dat.loc[hv_m_idx,'sex']='M'
+all_dat.loc[hv_f_idx,'sex']='F'
+
+
+#Use single letter for gender
+all_dat.sex.dropna(inplace=True)
+all_dat.drop(index=all_dat[all_dat.sex==-999].index, inplace=True)
+all_dat['sex']=all_dat['sex'].str[0].apply(str.upper)
+
+
+# #Remove run number from mmi task id
+# all_dat.loc[all_dat.type.str[0:4]=='mmi3','type']='mmi3'
+
+
+# =============================================================================
+# Demographic level stats
+# =============================================================================
+all_dat=all_dat.drop_duplicates(subset=['participant_id','SheetName'])
+# group_demo_data.age.hist(density=True)
+# group_demo_data.sex.hist()
+
+rename_dict=dict(ICA_1='NIHstr',
+              results_mous='MOUS',
+              NIH_hvprotocol='NIHhv',
+              results_camcan='CAMCAN',
+              omega = 'OMEGA') 
+
+all_dat.SheetName.replace(rename_dict, inplace=True)
+all_dat.rename(columns=dict(SheetName='Site'), inplace=True)
+all_dat.reset_index(inplace=True, drop=True)
---
+++ enigmeg/group/parse_OHMB_abstract_data.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 16:41:33 2023
+
+@author: jstout
+"""
+
+import os
+import pandas as pd
+topdir = '/home/jstout/src/enigma_OHBM/RESULTS_OHBM'
+os.chdir(topdir)
+
+def return_multisubject_dframe(filenames):
+    '''Take a list of filenames 
+    Load the csv filenames into a dataframe
+    Stitch together single subject dataframes into one large dataframe'''
+    
+    list_of_dframes = list()
+    for fname in filenames:
+        print(fname)
+        subj_dframe = pd.read_csv(fname, sep='\t')       
+        subj_dframe['subject'] = fname.split('/')[1].split('_')[0]
+        subj_dframe['group'] = fname.split('/')[0]
+        list_of_dframes.append(subj_dframe)
+        
+    group_dframe = pd.concat(list_of_dframes)
+    group_dframe = group_dframe.rename(columns={'Unnamed: 0':'Parcel'})
+    return group_dframe
+ 
+
+groups = ['MEGUK_card',
+     'CAMCAN',
+     'MOUS',
+     'OMEGA',
+     'NIH_hv',
+     'NIH_string']
+
+
+group_var=[]
+fnames=[]
+for group in groups:
+    tmp=glob.glob(f'{group}/*_Band_rel_power.csv')
+    fnames+= tmp
+    group_var += [group] * len(tmp)
+    
+dframe = pd.DataFrame(zip(group_var, fnames), columns=['group','fnames'])
+group = return_multisubject_dframe(fnames)
+group.reset_index(drop=True, inplace=True) #keep=False)
+ave_cats= ['[1, 3]', '[3, 6]', '[8, 12]', '[13, 35]', '[35, 55]',
+       'AlphaPeak']
+
+stats_dframe=group.groupby('Parcel')[ave_cats].mean()
+
+
+# =============================================================================
+# 
+# =============================================================================
+def filter_parcel_data(dframe, parcel_name=None, column_name=None):
+    '''Return single parcel information for stats testing'''
+    tmp=dframe[dframe.Parcel==parcel_name]
+    if ('age' in dframe.columns) and ('sex' in dframe.columns):
+        return tmp[[column_name, 'age', 'sex']].dropna()
+    else:
+        return tmp[[column_name]].dropna()
+
+def proc_parcel_regression(dframe):
+    '''Takes merged dataframe (subj_vars + parcel data)
+    and performs regression of variables using statsmodels'''
+    
+    #Initialize regression dataframe
+    rdframe = pd.DataFrame(columns=['parcel_name', 'coeff','rsquared_adj'])
+    rdframe.parcel_name = dframe.Parcel.unique()
+
+    for idx, row in rdframe.iterrows():
+        parc = row['parcel_name']
+        tmp=filter_parcel_data(dframe, parcel_name=parc, 
+                               column_name='AlphaPeak')
+        results = smf.ols('AlphaPeak ~ age', data=tmp).fit()
+        rdframe.loc[idx, 'rsquared_adj']=results.rsquared_adj
+        rdframe.loc[idx, 'coeff']=results.params['age']
+    
+    return rdframe
+
+
+def display_regression_coefs(stats_dframe, 
+                             subject_id='fsaverage',
+                             parc_name='aparc_sub',
+                             subjects_dir=None,
+                             image_outpath=None, 
+                             col_id=None, 
+                             rel_scaling=False, 
+                             fmin=None,
+                             fmax=None):
+    '''Plot the statistics on the brain
+    parc_name:
+        aparc or aparc_sub currently supported
+    
+    '''
+   
+    if subjects_dir != None: os.environ['SUBJECTS_DIR']=subjects_dir
+    hemi = "lh"
+    surf = "inflated"
+    
+    brain = Brain(subject_id, hemi, surf, background="white")
+    
+    aparc_file = os.path.join(os.environ["SUBJECTS_DIR"],
+                              subject_id, "label",
+                              hemi + f".{parc_name}.annot") 
+    labels, ctab, names = nib.freesurfer.read_annot(aparc_file)
+    
+    names2=[i.decode() for i in names] #convert from binary
+    if 'corpuscallosum' in names2: names2.remove('corpuscallosum')
+    if 'unknown' in names2: names2.remove('unknown')
+    
+    # Placeholder - must be larger than number of ROIs
+    roi_data = np.zeros(600)
+    roi_data[:] = np.nan
+    
+    for idx,name in enumerate(names2):
+        roi_data[idx]=stats_dframe.loc[name+'-'+hemi, col_id]
+    	
+    vtx_data = roi_data[labels]
+    vtx_data[labels == -1] = 0
+    
+    thresh=.001
+    vtx_data[np.abs(vtx_data)<thresh]=0
+    fmin = stats_dframe[col_id].min()
+    fmax = stats_dframe[col_id].max()
+    brain.add_data(vtx_data, colormap="jet", fmin=fmin, fmax=fmax)
+    
+    if image_outpath != None:
+        brain.save_image(image_outpath)
+
+# =============================================================================
+# Average images
+# =============================================================================
+#Alpha Peak
+display_regression_coefs(stats_dframe, col_id='AlphaPeak') 
+
+#Delta
+display_regression_coefs(stats_dframe, col_id='[1, 3]') 
+
+#Theta
+display_regression_coefs(stats_dframe, col_id='[3, 6]')
+
+#Alpha
+display_regression_coefs(stats_dframe, col_id='[8, 12]')
+
+#Beta
+display_regression_coefs(stats_dframe, col_id='[13, 35]')
+
+#Low Gamma
+display_regression_coefs(stats_dframe, col_id='[35, 55]')
+
+# =============================================================================
+# Regressor Images
+# =============================================================================
+#Load and fix names before merge
+demo_fname='/home/jstout/src/enigma_OHBM/RESULTS_OHBM/demographics.csv'
+demo_dframe = pd.read_csv(demo_fname)
+
+rename_dict=dict(NIHhv='NIH_hv',
+                 NIHstr='NIH_string')
+demo_dframe.Site.replace(rename_dict, inplace=True)
+
+#Merge
+final = pd.merge(demo_dframe, group, left_on=['Site','participant_id'], right_on=['group','subject'])
+#Required renaming because stats models does not accept numbers as names
+col_rename={'[1, 3]':'Delta',
+            '[3, 6]':'Theta',
+            '[8, 12]':'Alpha', 
+            '[13, 35]':'Beta',
+            '[35, 55]': 'L_Gamma'}
+final.rename(columns=col_rename, inplace=True)
+
+# Make regressors
+
+def proc_parcel_regression(dframe, col_id='AlphaPeak'):
+    '''Takes merged dataframe (subj_vars + parcel data)
+    and performs regression of variables using statsmodels'''
+    
+    #Initialize regression dataframe
+    rdframe = pd.DataFrame(columns=['Parcel', 'coeff','rsquared_adj'])
+    rdframe.Parcel = dframe.Parcel.unique()
+
+    for idx, row in rdframe.iterrows():
+        parc = row['Parcel'] #'parcel_name']
+        tmp=filter_parcel_data(dframe, parcel_name=parc, 
+                               column_name=col_id)
+        results = smf.ols(f'{col_id} ~ age', data=tmp).fit()
+        rdframe.loc[idx, 'rsquared_adj']=results.rsquared_adj
+        rdframe.loc[idx, 'coeff']=results.params['age']
+    rdframe.set_index('Parcel', inplace=True)
+    return rdframe
+
+
+#AlphaPeak
+rdframe=proc_parcel_regression(final.dropna(subset=['AlphaPeak']))
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+#Delta
+rdframe=proc_parcel_regression(final, col_id='Delta')
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+#Theta
+rdframe=proc_parcel_regression(final, col_id='Theta')
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+#Alpha
+rdframe=proc_parcel_regression(final, col_id='Alpha')
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+#Beta
+rdframe=proc_parcel_regression(final, col_id='Beta')
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+#L_Gamma
+rdframe=proc_parcel_regression(final, col_id='L_Gamma')
+display_regression_coefs(rdframe, col_id='rsquared_adj')
+
+# =============================================================================
+# Final number counts
+# =============================================================================
+test = final.groupby(['group']).subject.apply(set)
+for group, subject in test.iteritems():
+    print(f'{group} has {len(subject)}')
+    
+final.groupby(['group']).age.agg([np.mean, np.median, np.min, np.max])
+
+test2=final.groupby(['group', 'subject']).first()
+test2.reset_index(inplace=True)
+
+
+
