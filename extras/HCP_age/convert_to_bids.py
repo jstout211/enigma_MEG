be47f0ed5ef56325cde2783bae30216ecaeb7a4e
Jeff Stout <stoutjd@nih.gov>
2024-01-02 22:23:49 -0500

    FIX - hcp aging BIDS conversion

--- extras/HCP_age/convert_to_bids.py
+++ extras/HCP_age/convert_to_bids.py
@@ -15,17 +15,33 @@ from mne_bids import BIDSPath, write_raw_bids, write_anat
 import pandas as pd
 import logging
 
-logger = logging.Logger('base')
+logger=logging.getLogger()
+logdir  = op.join(os.getcwd(), 'logdir')
+if not op.exists(logdir):
+    os.mkdir(logdir)
 topdir='/fast/HCPPITT/sourcedata'
 os.chdir(topdir)
 
-dsets = glob.glob(op.join(topdir, 'HCP*','unprocessed','MEG','*','*.fif'))
-subjids = [i.split('sourcedata')[-1].split('/')[1] for i in dsets]
-dframe = pd.DataFrame(zip(dsets,subjids), columns=['fname','subjid'])
+def get_subj_logger(subjid, session, log_dir=None):
+     '''Return the subject specific logger.
+     This is particularly useful in the multiprocessing where logging is not
+     necessarily in order'''
+     fmt = '%(asctime)s :: %(levelname)s :: %(message)s'
+     sub_ses = f'{subjid}_ses_{session}'
+     subj_logger = logging.getLogger(sub_ses)
+     if subj_logger.handlers != []: # if not first time requested, use the file handler already defined
+         tmp_ = [type(i) for i in subj_logger.handlers ]
+         if logging.FileHandler in tmp_:
+             return subj_logger
+     else: # first time requested, add the file handler
+         fileHandle = logging.FileHandler(f'{log_dir}/{subjid}_ses-{session}_log.txt')
+         fileHandle.setLevel(logging.INFO)
+         fileHandle.setFormatter(logging.Formatter(fmt)) 
+         subj_logger.addHandler(fileHandle)
+         subj_logger.setLevel(logging.INFO)
+         subj_logger.info('Initializing subject level enigma_anonymization log')
+     return subj_logger   
 
-dframe['task']= dframe.fname.apply(get_dset_info, **{'return_type':'task'})
-dframe['run']= dframe.fname.apply(get_dset_info, **{'return_type':'run'})
-dframe['subjects_dir']=topdir + '/' +  dframe.subjid  + '/T1w'
 
 def get_dset_info(fname, return_type=None):
     tasktype=op.basename(op.dirname(fname))
@@ -63,18 +79,44 @@ def do_auto_coreg(raw_fname, subject, subjects_dir):
     coreg.fit_icp(n_iterations=6, nasion_weight=.5, hsp_weight= 5, verbose=True)
     return coreg.trans
     
+
+dsets = glob.glob(op.join(topdir, 'HCP*','unprocessed','MEG','*','*raw.fif'))
+subjids = [i.split('sourcedata')[-1].split('/')[1] for i in dsets]
+dframe = pd.DataFrame(zip(dsets,subjids), columns=['fname','subjid'])
+
+dframe['task']= dframe.fname.apply(get_dset_info, **{'return_type':'task'})
+dframe['run']= dframe.fname.apply(get_dset_info, **{'return_type':'run'})
+dframe['subjects_dir']=topdir + '/' +  dframe.subjid  + '/T1w'
+dframe['session'] = '1'
+
+for idx, row in dframe.iterrows():
+    if row.subjid[-2:] == 'V2':
+        dframe.loc[idx,'subjid'] = row.subjid[:-2]
+        dframe.loc[idx, 'session'] = '2'
+
+csv_outfname = op.join(logdir, 'dframe.csv')
+if op.exists(csv_outfname):
+    os.remove(csv_outfname)
+dframe.to_csv(op.join(logdir, 'dframe.csv'))
     
 # =============================================================================
 #  Make Transform from automated fit
 # =============================================================================
 failed=[]
 for idx,row in dframe.iterrows():
+    logger = get_subj_logger(row.subjid, row.session, log_dir=logdir)
     try:
         if (row.run==1) & (row.task not in ['empty','closed']):
+            logger.info(f'Estimating Transform')
             trans = do_auto_coreg(row.fname, row.subjid, row.subjects_dir)
-            trans.save(op.join(op.dirname(op.dirname(row.fname)), f'{row.subjid}_trans.fif'))
-    except:
+            dframe.loc[idx,'trans_fname']=op.join(op.dirname(op.dirname(row.fname)), f'{row.subjid}_{row.session}_trans.fif')
+            if op.exists(dframe.loc[idx,'trans_fname']):
+                os.remove(dframe.loc[idx,'trans_fname'])
+            trans.save(dframe.loc[idx,'trans_fname'])
+            logger.info(f'Successfully saved trans file')
+    except BaseException as e:
         print(row.fname)
+        logger.exception(str(e))
         failed.append(row.fname)
 
 
@@ -84,56 +126,62 @@ for idx,row in dframe.iterrows():
 
 bids_dir = op.join(topdir, 'BIDS')
 for idx,row in dframe.iterrows():
+    logger = get_subj_logger(row.subjid, row.session, log_dir=logdir)
     try:
+        logger.info(f'Starting MEG BIDS')
         trans_fname = op.join(op.dirname(op.dirname(row.fname)), f'{row.subjid}_trans.fif')
         trans = mne.read_trans(trans_fname)
         raw = mne.io.read_raw_fif(row.fname)
         raw.info['line_freq'] = 60 
-        if row.subjid[-2:]=='v2':
-            ses='2'
-        else:
-            ses = '1'
+        ses = row.session
         run = row.run
         run = str(run) 
         if len(run)==1: run='0'+run
         bids_path = BIDSPath(subject=row.subjid, session=ses, task=row.task,
                               run=run, root=bids_dir, suffix='meg')
         write_raw_bids(raw, bids_path, overwrite=True)
-        logger.info(f'Successful MNE BIDS: {meg_fname} to {bids_path}')
+        logger.info(f'Successful MEG BIDS: {bids_path.fpath}')
     except BaseException as e:
         print(row.fname)
-        logger.info(f'failed BIDS:  {str(e)}') 
+        logger.exception(f'failed MEG BIDS:  {str(e)}') 
 
 # =============================================================================
 # MRI BIDS
 # =============================================================================
 
 for idx,row in dframe.iterrows():
+    logger = get_subj_logger(row.subjid, row.session, log_dir=logdir)
     if (row.task != 'eyesopen') & (row.run != 1):
+        logger.info(f'Ignoring {row.fname}')
         continue
     else:
-        raw = mne.io.read_raw_fif(row.fname)
-        trans_fname = op.join(op.dirname(op.dirname(row.fname)), f'{row.subjid}_trans.fif')
-        trans = mne.read_trans(trans_fname)
+        try:
+            raw = mne.io.read_raw_fif(row.fname)
+            trans_fname = row.trans_fname #op.join(op.dirname(op.dirname(row.fname)), f'{row.subjid}_trans.fif')
+            trans = mne.read_trans(trans_fname)
+            
+            t1w_bids_path = \
+                BIDSPath(subject=row.subjid, session=row.session, root=bids_dir, suffix='T1w')
         
-        ses='1'
-        t1w_bids_path = \
-            BIDSPath(subject=row.subjid, session=ses, root=bids_dir, suffix='T1w')
-    
-        landmarks = mne_bids.get_anat_landmarks(
-            image=op.join(row.subjects_dir, row.subjid, 'mri','T1.mgz'),
-            info=raw.info,
-            trans=trans,
-            fs_subject=row.subjid,
-            fs_subjects_dir=row.subjects_dir
-            )
-        
-        # Write regular
-        t1w_bids_path = write_anat(
-            image=op.join(row.subjects_dir, row.subjid, 'mri','T1.mgz'),
-            bids_path=t1w_bids_path,
-            landmarks=landmarks,
-            deface=False, 
-            overwrite=True
-            )
+            landmarks = mne_bids.get_anat_landmarks(
+                image=op.join(row.subjects_dir, row.subjid, 'mri','T1.mgz'),
+                info=raw.info,
+                trans=trans,
+                fs_subject=row.subjid,
+                fs_subjects_dir=row.subjects_dir
+                )
+            logger.info(f'Calc-ed landmarks')
+            
+            # Write regular
+            t1w_bids_path = write_anat(
+                image=op.join(row.subjects_dir, row.subjid, 'mri','T1.mgz'),
+                bids_path=t1w_bids_path,
+                landmarks=landmarks,
+                deface=False, 
+                overwrite=True
+                )
+            logger.info(f'Successful MRI BIDS: {t1w_bids_path.fpath}')
+        except BaseException as e:
+            logger.exception(str(e))
+            
 
