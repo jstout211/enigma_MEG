e898f5914b1f0b0030a6b61d887e886b2450de74
Jeff Stout <stoutjd@nih.gov>
2024-01-16 21:47:05 -0500

    FIX - mri naming

--- enigmeg/test/test_process_meg.py
+++ enigmeg/test/test_process_meg.py
@@ -108,7 +108,6 @@ def test_do_spectral_param():
 
 #%%  Test CSV input
 
-
 def test_parse_bids(tmp_path):
     test_id = 'ON02747'
     d = tmp_path / "parse_bids"
@@ -130,13 +129,19 @@ def test_parse_bids(tmp_path):
     assert np.all(row.type == '.ds')
     meg_gt = op.join(bids_root, 'sub-'+test_id, 'ses-01','meg',f'sub-{test_id}_ses-01_task-rest_run-01_meg.ds')
     assert np.all(row.path == meg_gt)
-    mri_gt = op.join(bids_root, 'sub-'+test_id, 'ses-01','anat',f'sub-{test_id}_ses-01_acq-MPRAGE_rec-SCIC_T1w.nii.gz')
-    assert np.all(row.mripath == mri_gt)
+    #HACK to determine MRI file -- multiple T1w files with same coreg
+    mri_gt_dir = op.join(bids_root, 'sub-'+test_id, 'ses-01','anat')
+    assert np.all(op.dirname(row.mripath.to_string(index=False)) == mri_gt_dir)
+    tmp_ = op.basename(row.mripath.to_string(index=False))
+    assert tmp_[0:30]==f'sub-{test_id}_ses-01_acq-MPRAGE_'
+    assert tmp_[-10:]=='T1w.nii.gz'
 
 
-
-# def test_csv_procmeg(tmp_path):
+def test_csv_procmeg(tmp_path):
+    csv_file = tmp_path / "parse_bids" / "tmp_parse_bids" / "ParsedBIDS_dataframe.csv"
+    cmd_ = 'process_meg.py -bids_root {bids_root} -mains 60 -n_jobs 1 -proc_fromcsv {csv_file}'
+    subprocess.call(cmd_.split())
     
-#     tmp_ = 'process_meg.py -bids_root {bids_root} -mains 60 -n_jobs 1 -proc_fromcsv ParsedBIDS_dataframe.csv'
     
 
+tmp_ = '/home/jstout/ds004215/sub-ON02747/ses-01/anat/sub-ON02747_ses-01_acq-MPRAGE_rec-SCIC_T1w.nii.gz'
