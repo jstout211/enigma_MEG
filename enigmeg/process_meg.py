1045561af7e8b2a0ae721375853705537d094481
Jeff Stout <stoutjd@nih.gov>
2024-01-25 20:08:14 -0500

    Remove preproc=False from the ICA call
    Preprocessing is handled by MEGnet for consistency, and the raw MNE object is copied internally

--- enigmeg/process_meg.py
+++ enigmeg/process_meg.py
@@ -475,8 +475,7 @@ class process():
         bad_channels = [i for i in self.bad_channels if i in self.raw_rest.info['ch_names']] #Prevent drop channels from erroring
         ICA(self.raw_rest,mains_freq=float(self.proc_vars['mains']), 
             save_preproc=True, save_ica=True, results_dir=self.deriv_path.directory, 
-            outbasename=ica_basename, do_assess_bads=False, bad_channels=bad_channels, 
-            run_preproc=False)  
+            outbasename=ica_basename, do_assess_bads=False, bad_channels=bad_channels)  
         self.fnames.ica_folder = self.deriv_path.directory  / ica_basename
         self.fnames.ica = self.fnames.ica_folder / (ica_basename + '_0-ica.fif')
         self.fnames.ica_megnet_raw =self.fnames.ica_folder / (ica_basename + '_250srate_meg.fif')
